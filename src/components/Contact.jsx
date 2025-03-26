eb75e9b76b7000ad3d8ba1fddb96f53feeee4f45
Stefan Pla <spla@icloud.com>
2025-03-26 12:32:59 -0400

    got shuffling images onto contact page, messed around with the css there a little too

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import '../styles/Contact.css'
+import ContactPageCarousel from './ContactPageCarousel';
 import HomeButton from '../components/HomeButton';
 
 function Contact() {
@@ -8,9 +9,9 @@ function Contact() {
   return (
     <div className="contact-page">
       <div className="contact-text">
-        <h3>Contact Me</h3>
-        <p>I'd love to hear from you! Click the button below to send me an email.</p>
-        <a href={`mailto:${email}`} className="email-button">Email Me</a>
+        <p>jared.brayton.bollenbacher@gmail.com </p>
+        <p>brayton@cgmc.com</p>
+        <ContactPageCarousel />
         <HomeButton />
       </div>
     </div>
